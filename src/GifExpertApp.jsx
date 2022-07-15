80ac9715926621e0abcec29acf9ac2408fa6b317
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-15 09:29:02 -0500

    Creación de lista de categorías

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball']);
+    console.log(categories);
+
   return (
     <>
+        {/* titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* input */}
+
+        {/* Listado de gif */}
+        <ol>
+            {
+                categories.map( category => {
+                    return <li key= { category }>{category}</li>
+                })
+            }
+        </ol>
     </>
   )
 }
