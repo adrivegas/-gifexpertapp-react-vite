a3a401131ec9f1859157587b71c825510c3eb946
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-19 20:09:52 -0500

    Pruebas en el componente GifItem

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -11,4 +11,23 @@ describe('Pruebas en GifItem', () => {
         const { container } = render(<GifItem title={title} url={url} />);
         expect(container).toMatchSnapshot();
     });
+
+    test('debe mostrar la imágen con el URL y el ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+        // screen.debug();
+        // expect( screen.getByRole('img').src).toBe(url);
+        // expect( screen.getByRole('img').alt).toBe(title);
+
+        // Mejor forma de hacerlo:
+        const { src, alt } = screen.getByRole('img');
+        expect( src).toBe(url);
+        expect( alt).toBe(title);
+
+    });
+
+    test('debe de mostrar el título en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect( screen.getAllByText( title )).toBeTruthy();
+    });
+
 });
