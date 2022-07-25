a09fc35dfd000647af115116f6a5e3cf4530a452
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-25 09:48:09 -0500

    Inicio de pruebas eb GifExpertApp y actualización de docs

---
+++ test/GifExpertApp.test.jsx
@@ -0,0 +1,16 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { GifExpertApp } from '../src/GifExpertApp';
+
+
+describe('Pruebas en <GifExpertApp />', () => {
+
+    const inputValue = 'Spider';
+
+    test('should ', () => {
+        
+        render( <GifExpertApp /> );
+        screen.debug();
+
+
+    });
+});
